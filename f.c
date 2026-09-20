089aa84ea4d52f9afa15aaca60cd7166641540ff
hritwikaroy03-a11y <hritwikaroy03@gmail.com>
2026-09-20 16:53:20 +0600

    Add files via upload

---
+++ f.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int a,b,c;
+    scanf("%d %d %d",&a,&b,&c);
+    if(a>b&&a>c){
+        printf("a is the largest");
+    }else if(b>a&&b>c){
+        printf("b is the largest");
+    }else{
+        printf("c is the largest");
+    }
+}
